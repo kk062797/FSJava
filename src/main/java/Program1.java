e0c09753af91e6e9c64b4b782d4fe5d976474c04
kk062797 <kanishk423@gmail.com>
2024-10-27 19:57:35 +0530

    day3

--- src/main/java/Program1.java
+++ src/main/java/Program1.java
@@ -1,6 +1,14 @@
 public class Program1 {
 
     public static void main(String [] args){
+
         System.out.println("My Name is Kanishk");
+        sum(5,6);
     }
+
+
+public static void sum(int a, int b) {
+    int c = a + b;
+    System.out.println("Sum is: " + c);
+}
 }
