b664cdf70aa94a5d236797a759b577da2c8a6ec5
kk062797 <kanishk423@gmail.com>
2024-10-28 19:25:13 +0530

    Day5&Day6 changes

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,29 +1,30 @@
 public class Calculator {
 
-    public static void main(String[] args) {
+        public static void main(String[] args) {
 
-        Calculator c1 = new Calculator();
-        System.out.println(c1.add(5,6));
-        System.out.println(c1.sub(10,3));
-        System.out.println(c1.mul(34,27));
-        System.out.println(c1.div(47,4));
+            Calculator c1 = new Calculator();
+            System.out.println(c1.add(5,6));
+            System.out.println(c1.sub(10,3));
+            System.out.println(c1.mul(34,27));
+            System.out.println(c1.div(47,4));
+
+        }
+
+        public int add(int a, int b){
+            return a+b;
+        }
+
+        public int sub(int a, int b){
+            return a-b;
+        }
+
+        public int mul(int a, int b){
+            return a*b;
+        }
+
+        public float div(int a, int b){
+            return a/b;
+        }
 
     }
 
-    public int add(int a, int b){
-        return a+b;
-    }
-
-    public int sub(int a, int b){
-        return a-b;
-    }
-
-    public int mul(int a, int b){
-        return a*b;
-    }
-
-    public float div(int a, int b){
-        return a/b;
-    }
-
-}
